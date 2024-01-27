3588d3cfb44403c4c8b4678798eb4b6cbeb84c23
AtharvaKitkaru <atharvakitkaru01@gmail.com>
2024-01-27 15:35:12 +0530

    made home mobile responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Separator from "../common/Separator";
 
 function Home() {
   return (
-    <div className="m-0 p-0  w-full h-[100vh] flex flex-col-reverse md:flex-row">
+    <div className="m-0 p-0  page flex flex-col-reverse md:flex-row">
       <div className="flex-1 m-auto text-center max-h-screen flex-column justify-center align-middle">
-        <div className="text-4xl font-bold uppercase  m-auto text-slate-800 dark:text-white">
-          I am <span className="text-[#c3a342] dark:text-[#ffbf00]">Atharva Kitkaru.</span>
+        <div className="text-3xl md:text-4xl text-wrap font-bold uppercase m-auto text-slate-800 dark:text-white pt-5 md:pt-0">
+          I am{" "}
+          <span className="text-[#c3a342] dark:text-[#ffbf00]">
+            Atharva Kitkaru.
+          </span>
         </div>
         <div className="mt-4 w-4/5 m-auto font-sofia">
           Versatile professional with experience in Frontend and Backend
@@ -15,10 +17,10 @@ function Home() {
       </div>
 
       <div
-        className="flex-1 m-auto bg-no-repeat bg-fill bg-center md:h-full sm:h-auto sm:w-auto flex justify-center align-middle"
+        className="flex-1 m-auto bg-no-repeat bg-fill bg-cover w-full h-1/2 md:h-screen flex justify-center align-middle"
         style={{ backgroundImage: "url('/blob.png')" }}
       >
-        <img className="m-auto" src="atharva.png" alt="Atharva" />
+        <img className="m-auto h-1/3" src="atharva.png" alt="Atharva" />
       </div>
     </div>
   );
