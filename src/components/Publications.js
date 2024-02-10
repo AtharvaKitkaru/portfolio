ac057f85e9381ee131eb62c49a897be9821040a7
AtharvaKitkaru <atharvakitkaru01@gmail.com>
2024-02-10 16:18:35 +0530

    adjusted modal

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -103,13 +103,13 @@ const Publications = () => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             onClick={() => setPublicationsModalOpen(false)}
-            className="bg-slate-900/20 backdrop-blur p-8 fixed inset-0 z-50 grid place-items-center overflow-y-scroll cursor-pointer "
+            className="bg-slate-900/20 backdrop-blur p-8 fixed inset-0 z-50 grid place-items-center overflow-y-scroll cursor-pointer min-w-4/5 md:w-auto"
           >
             <motion.div
               initial={{ scale: 0, rotate: "12.5deg" }}
               animate={{ scale: 1, rotate: "0deg" }}
               exit={{ scale: 0, rotate: "0deg" }}
-              transition={{ duration: 0.5, ease: easeIn }}
+              transition={{ duration: 0.35, ease: easeIn }}
               onClick={(e) => e.stopPropagation()}
               className="bg-gradient-to-br from-copy to-copy-light text-white p-6 rounded-lg w-full max-w-lg shadow-xl cursor-default relative"
             >
